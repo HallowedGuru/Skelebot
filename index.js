73b09be22dc2bf220d4ea4367f14c04e0fdb42d2
jfagan132 <jfagan@ggc.edu>
2018-08-09 20:32:21 -0400

    adjuested ily command so that skelebot replies as long as the message begins with 'ily'

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('message', message => {
 		else if (command === "freak") {
 			message.channel.send("https://www.youtube.com/watch?v=pdQiSGb4Luw");
 		}
-		else if (command === "ily") {
+		else if (command.startsWith(("ily ")) {
 			message.reply("ily2");
 		}
 		else if (command.startsWith("random ")) {
